1c8596260c2d77b434d631d5b3c19d6453d8586a
nickonometry <nick.andrews@tylertech.com>
2022-02-24 07:55:35 -0500

    wired up projects page KHEDDDDD

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -117,8 +117,8 @@
 				<li class:active={$page.url.pathname === '/about'}>
 					<a sveltekit:prefetch href="/about">About</a>
 				</li>
-				<li class:active={$page.url.pathname === '/todos'}>
-					<a sveltekit:prefetch href="/todos">Todos</a>
+				<li class:active={$page.url.pathname === '/projects'}>
+					<a sveltekit:prefetch href="/projects">Projects</a>
 				</li>
 			</ul>
 		</nav>
--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -1,1 +1,6 @@
 <h1>Projects</h1>
+
+<!-- TO DO - pull project ID/Name from API and pass it into URL -->
+<a href="/projects/project-1">Project 1</a>
+<a href="/projects/project-2">Project 2</a>
+<a href="/projects/project-3">Project 3</a>
