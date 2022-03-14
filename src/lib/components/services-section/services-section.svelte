7181bc6cfbb78e090416473bba5f3ae700a305ff
nickonometry <nick.andrews@tylertech.com>
2022-03-14 13:05:05 -0400

    added some action links to cards

--- src/lib/components/services-section/services-section.svelte
+++ src/lib/components/services-section/services-section.svelte
@@ -9,40 +9,73 @@
 <section data-aos="fade">
 	<article>
 		<div data-aos="fade-up" data-aos-delay="100" class="article__inner">
-			<img src="images/home/undraw_at_work_re_qotl.svg" alt="" />
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+				><path
+					d="M352 96C352 110.3 348.9 123.9 343.2 136.2L396 227.4C372.3 252.7 341.9 271.5 307.6 281L256 192H255.1L187.9 309.5C209.4 316.3 232.3 320 256 320C326.7 320 389.8 287.3 430.9 235.1C441.9 222.2 462.1 219.1 475.9 231C489.7 242.1 491.9 262.2 480.8 276C428.1 341.8 346.1 384 256 384C220.6 384 186.6 377.6 155.3 365.9L98.65 463.7C93.95 471.8 86.97 478.4 78.58 482.6L23.16 510.3C18.2 512.8 12.31 512.5 7.588 509.6C2.871 506.7 0 501.5 0 496V440.6C0 432.2 2.228 423.9 6.46 416.5L66.49 312.9C53.66 301.6 41.84 289.3 31.18 276C20.13 262.2 22.34 242.1 36.13 231C49.92 219.1 70.06 222.2 81.12 235.1C86.79 243.1 92.87 249.8 99.34 256.1L168.8 136.2C163.1 123.9 160 110.3 160 96C160 42.98 202.1 0 256 0C309 0 352 42.98 352 96L352 96zM256 128C273.7 128 288 113.7 288 96C288 78.33 273.7 64 256 64C238.3 64 224 78.33 224 96C224 113.7 238.3 128 256 128zM372.1 393.9C405.5 381.1 435.5 363.2 461.8 341L505.5 416.5C509.8 423.9 512 432.2 512 440.6V496C512 501.5 509.1 506.7 504.4 509.6C499.7 512.5 493.8 512.8 488.8 510.3L433.4 482.6C425 478.4 418.1 471.8 413.3 463.7L372.1 393.9z"
+				/></svg
+			>
 			<h3>Our services</h3>
 			<p>
 				We draw on expertise from across the entire project scope, including engineering,
 				procurement, fabrication, construction, 3D coordination and installation. Our comprehensive
 				services have transformed project execution.
 			</p>
-			<a href="request-pricing">Request pricing</a>
+			<a href="request-pricing"
+				>Request Pricing
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+					><path
+						d="M512 256c0-141.4-114.6-256-256-256S0 114.6 0 256c0 141.4 114.6 256 256 256S512 397.4 512 256zM265.9 382.8C259.9 380.3 256 374.5 256 368v-64H160c-17.67 0-32-14.33-32-32v-32c0-17.67 14.33-32 32-32h96v-64c0-6.469 3.891-12.31 9.875-14.78c5.984-2.484 12.86-1.109 17.44 3.469l112 112c6.248 6.248 6.248 16.38 0 22.62l-112 112C278.7 383.9 271.9 385.3 265.9 382.8z"
+					/>
+				</svg>
+			</a>
 		</div>
 	</article>
 
 	<article>
 		<div data-aos="fade-up" data-aos-delay="200" class="article__inner">
-			<img src="images/home/undraw_features_overview.svg" alt="" />
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
+				><path
+					d="M0 80C0 53.49 21.49 32 48 32H144C170.5 32 192 53.49 192 80V96H384V80C384 53.49 405.5 32 432 32H528C554.5 32 576 53.49 576 80V176C576 202.5 554.5 224 528 224H432C405.5 224 384 202.5 384 176V160H192V176C192 177.7 191.9 179.4 191.7 180.1L272 288H368C394.5 288 416 309.5 416 336V432C416 458.5 394.5 480 368 480H272C245.5 480 224 458.5 224 432V336C224 334.3 224.1 332.6 224.3 331L144 224H48C21.49 224 0 202.5 0 176V80z"
+				/></svg
+			>
 			<h3>Featured Projects</h3>
 			<p>
 				We've given our clients the confidence that we can deliver complex, logistically
 				challenging, and technically difficult projects on time while maintaining safety as our #1
 				priority.
 			</p>
-			<a href="request-pricing">Request pricing</a>
+			<a href="request-pricing"
+				>View Projects
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+					><path
+						d="M512 256c0-141.4-114.6-256-256-256S0 114.6 0 256c0 141.4 114.6 256 256 256S512 397.4 512 256zM265.9 382.8C259.9 380.3 256 374.5 256 368v-64H160c-17.67 0-32-14.33-32-32v-32c0-17.67 14.33-32 32-32h96v-64c0-6.469 3.891-12.31 9.875-14.78c5.984-2.484 12.86-1.109 17.44 3.469l112 112c6.248 6.248 6.248 16.38 0 22.62l-112 112C278.7 383.9 271.9 385.3 265.9 382.8z"
+					/>
+				</svg>
+			</a>
 		</div>
 	</article>
 
 	<article>
 		<div data-aos="fade-up" data-aos-delay="300" class="article__inner">
-			<img src="images/home/undraw_career_development.svg" alt="" />
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"
+				><path
+					d="M224 256c70.7 0 128-57.31 128-128S294.7 0 224 0C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3c-95.73 0-173.3 77.6-173.3 173.3C0 496.5 15.52 512 34.66 512H413.3C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304zM479.1 320h-73.85C451.2 357.7 480 414.1 480 477.3C480 490.1 476.2 501.9 470 512h138C625.7 512 640 497.6 640 479.1C640 391.6 568.4 320 479.1 320zM432 256C493.9 256 544 205.9 544 144S493.9 32 432 32c-25.11 0-48.04 8.555-66.72 22.51C376.8 76.63 384 101.4 384 128c0 35.52-11.93 68.14-31.59 94.71C372.7 243.2 400.8 256 432 256z"
+				/></svg
+			>
 			<h3>New Careers</h3>
 			<p>
 				Tech Air the leader in innovation and employment growth within the HVAC industry. We design
 				and build challenging and unique projects that are re-shaping the landscape of New England.
 				Come build a better future with us.
 			</p>
-			<a href="request-pricing">Request pricing</a>
+			<a href="request-pricing"
+				>Career Details
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+					><path
+						d="M512 256c0-141.4-114.6-256-256-256S0 114.6 0 256c0 141.4 114.6 256 256 256S512 397.4 512 256zM265.9 382.8C259.9 380.3 256 374.5 256 368v-64H160c-17.67 0-32-14.33-32-32v-32c0-17.67 14.33-32 32-32h96v-64c0-6.469 3.891-12.31 9.875-14.78c5.984-2.484 12.86-1.109 17.44 3.469l112 112c6.248 6.248 6.248 16.38 0 22.62l-112 112C278.7 383.9 271.9 385.3 265.9 382.8z"
+					/>
+				</svg>
+			</a>
 		</div>
 	</article>
 </section>
@@ -76,16 +109,35 @@
 		border-bottom: 4px solid transparent;
 		transition: border-bottom ease-in 100ms;
 
-		.article__inner {
-			display: grid;
-			grid-template-columns: 1fr;
-			grid-template-rows: auto auto 1fr auto;
-			height: 100%;
+		.article {
+			&__inner {
+				display: grid;
+				grid-template-columns: 1fr;
+				grid-template-rows: auto auto 1fr auto;
+				height: 100%;
+
+				a {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					height: 2rem;
+
+					svg {
+						height: 24px;
+						width: 24px;
+						margin-left: 1rem;
+						fill: var(--accent-color);
+					}
+				}
+			}
 		}
 
-		img {
+		svg {
 			width: 100%;
-			max-height: 120px;
+			height: 120px;
+			fill: transparent;
+			stroke: var(--accent-color);
+			stroke-width: 8px;
 		}
 
 		h3 {
