e711b0b5095e268b06be785dd5fe71f9b78b8884
Adam Quinlan <quinlanadam@gmail.com>
2022-03-26 22:08:30 -0400

    linked contact button

--- src/lib/components/slick-button/slick-button.svelte
+++ src/lib/components/slick-button/slick-button.svelte
@@ -1,12 +1,14 @@
 <div class="button-container">
 	<div class="center">
-		<button class="btn">
-			<svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
-				<polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
-				<polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
-			</svg>
-			<span>Contact Us</span>
-		</button>
+		<a href="/contact">
+			<button class="btn">
+				<svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+					<polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+					<polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+				</svg>
+				<span>Contact Us</span>
+			</button>
+		</a>
 	</div>
 </div>
 
