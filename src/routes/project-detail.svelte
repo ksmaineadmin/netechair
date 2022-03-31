b368ef288edaa5a1ca49b625333f9f3312d13fb8
nickonometry <nick.andrews@tylertech.com>
2022-03-31 10:59:50 -0400

    created project detail page and made it responsive

--- src/routes/project-detail.svelte
+++ src/routes/project-detail.svelte
@@ -4,10 +4,10 @@
 	onMount(() => {});
 </script>
 
+<div class="hero__container">
+	<h1 class="hero__title">Portfolio Item Title</h1>
+</div>
 <section>
-	<div class="hero__container">
-		<h1 class="hero__title">Portfolio Item Title</h1>
-	</div>
 	<div class="portfolio__container">
 		<div class="portfolio__details">
 			<h2 class="portfolio__title">Project Description</h2>
@@ -20,12 +20,27 @@
 				passages, and more recently with desktop publishing software like Aldus PageMaker including
 				versions of Lorem Ipsum.
 			</p>
+			<div class="divider" />
+			<h2 class="portfolio__title">Client</h2>
+			<p>Client name here</p>
+			<div class="divider" />
+			<h2 class="portfolio__title">Project Date</h2>
+			<p>27 August 2020</p>
 		</div>
 		<div class="portfolio__gallery">
-			Images here
-			<img src="" />
-			<img src="" />
-			<img src="" />
+			<img
+				src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+				class="span-full"
+				alt=""
+			/>
+			<img
+				src="https://images.unsplash.com/photo-1522683845419-f46a59d0de42?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+				alt=""
+			/>
+			<img
+				src="https://images.unsplash.com/photo-1455165814004-1126a7199f9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+				alt=""
+			/>
 		</div>
 	</div>
 </section>
@@ -36,6 +51,7 @@
 
 	.hero {
 		&__container {
+			margin-top: 120px;
 			display: flex;
 			align-items: center;
 			justify-content: center;
@@ -65,16 +81,73 @@
 
 	.portfolio {
 		&__container {
+			padding: 3rem;
 			max-width: $device-large;
 			margin: 0 auto;
 			display: grid;
 			grid-template-columns: 4fr 8fr;
-			gap: 1rem;
+			gap: 2rem;
 			justify-content: center;
+			width: 100%;
+		}
+
+		&__details {
+			p {
+				font-size: 1.075rem;
+				color: var(--secondary-font-color);
+			}
 		}
 		&__title {
-			margin-bottom: 2rem;
+			margin-bottom: 1rem;
 			font-size: 1.5rem;
+			color: var(--accent-color);
+		}
+
+		&__gallery {
+			width: 100%;
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			gap: 1rem;
+
+			img {
+				max-width: 100%;
+			}
+		}
+	}
+
+	.divider {
+		height: 2px;
+		width: 1rem;
+		background-color: var(--secondary-font-color);
+		opacity: 0.4;
+		margin: 3.5rem 0;
+	}
+
+	@media screen and (max-width: $device-medium) {
+		.portfolio {
+			&__container {
+				grid-template-columns: 1fr;
+			}
+
+			&__details {
+				p {
+					font-size: 1.075rem;
+					color: var(--secondary-font-color);
+				}
+			}
+			&__title {
+				margin-bottom: 1rem;
+				font-size: 1.5rem;
+				color: var(--accent-color);
+			}
+
+			&__gallery {
+				grid-template-columns: 1fr;
+
+				img {
+					max-width: 100%;
+				}
+			}
 		}
 	}
 </style>
